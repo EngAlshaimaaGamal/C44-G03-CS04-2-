54fe94db98c6a34d2b27295ecd183cc1d74325ac
AlshaimaaGamal <shaimaa.20377393@compit.aun.edu.eg>
2025-07-12 16:37:36 +0200

    Evolution C#7.0

--- Demoo/Program.cs
+++ Demoo/Program.cs
@@ -4,7 +4,131 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Evalution in switch in c# 
+
+
+            #region Evolution in c# 7.0
+
+           // object number = 5.6; // boxing
+
+            #region Example1[ pattern maching on types]
+            //// no jump table will be create [Bid number of any type]
+            //switch (number)
+            //{
+            //    case int value: // unboxing
+            //        Console.WriteLine("int");
+            //        break;
+            //    case double value:
+            //        Console.WriteLine("double");
+            //        break;
+            //    case float value:
+            //        Console.WriteLine("float");
+            //        break;
+            //    default:
+            //        Console.WriteLine("No Match");
+            //        break;
+
+            //}
+
+
+
+
+
+
+
+
+
+
+            #endregion
+
+            #region Example 2 [Case Guard when]
+
+            //switch (number)
+            //{
+            //    case int value when value <10: // unboxing
+            //        Console.WriteLine("int less than 10");
+            //        break;
+            //    case int value when value > 10: // unboxing
+            //        Console.WriteLine("int Bigger than 10");
+            //        break;
+            //    case decimal value:
+            //        Console.WriteLine("decimal");
+            //        break;
+            //    case double value when value >5.5 && value < 10:
+            //        Console.WriteLine("Double Between 5.5 to 10");
+            //        break;
+            //    default:
+            //        Console.WriteLine("No Match");
+            //        break;
+
+            //}
+
+
+
+
+
+
+            #endregion
+
+            #region Example 3 [user -Defined DateType]
+
+                    //object num = 50;
+                    //num = new person();
+                    //{ Id = 1 , name = "Omer " ,Age = 25}
+
+                    //switch (num)
+                    // {
+                    //    case int value when value < 10:
+                    //        Console.WriteLine("int");
+                    //        break ;
+                    //        case string value:
+                    //        Console.WriteLine("String");
+                    //        break ; 
+                    //        case double value:
+                    //        Console.WriteLine("double");
+                    //        break ;
+                    //        case person  value when ID == 1 && name == "omer" : // short cricut
+                    //        Console.WriteLine("person");
+                    //        break ; 
+                    //        default:
+                    //        Console.WriteLine("No match");
+                    //        break ; 
+            
+            
+              
+            
+            
+                    //  }
+
+
+
+            #endregion
+
+
+
+
+
+
+
+
+            #endregion
+
+
+
+
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
